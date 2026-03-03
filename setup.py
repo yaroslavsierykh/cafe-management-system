291fb3e49e01ccd6be4cb94a6d09eae2bbd531ae
Yaroslav Sierykh <yaroslav101serykh@gmail.com>
2026-03-03 14:03:41 +0200

    Refactor dependencies to use parse_requirements function

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
+
 setup(
     name='cafe-management',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'flask==3.0.2',
-        'flask-sqlalchemy',
-        'flask-login',
-        'werkzeug'
-    ]
-) 
+    install_requires=parse_requirements('requirements.txt'),
+)
